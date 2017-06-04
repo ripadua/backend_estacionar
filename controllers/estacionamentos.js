2f83a849db60be0b41e21c8c0eaf99b776dc6c40
ricardocrispim <ripadua7@gmail.com>
2017-06-04 16:58:58 -0300

    Criação do backend de despesas

--- controllers/estacionamentos.js
+++ controllers/estacionamentos.js
@@ -58,6 +58,25 @@ module.exports = function(app) {
 
 	});
 
+	app.get('/estacionamentos/:id/despesas', function(req, res){
+		console.log('Processando uma listagem de despesas de um estacionamento.');
+
+		var id = req.params.id;
+
+		var connection = app.persistence.connectionFactory();
+		var despesaDAO = new app.persistence.DespesaDAO(connection);
+
+		despesaDAO.listaPorIdEstacionamento(id, function(erro, resultado){
+			if (erro) {
+				console.log('Erro ao consultar lista de despesas de um estacionamento: ' + erro);
+				res.status(500).send(erro);
+			} else {
+				res.status(200).json(resultado);
+			}
+		});
+
+	});
+
 	app.post('/estacionamentos', function(req, res){
 		console.log('Processando uma inclusão de estacionamento.');
 
@@ -74,12 +93,7 @@ module.exports = function(app) {
 
 		var estacionamento = req.body["estacionamento"];
 		
-		if (estacionamento.id) {
-			estacionamento.datahora_alteracao = new Date();
-			delete estacionamento.datahora_inclusao;
-		} else {
-			estacionamento.datahora_inclusao = new Date();
-		}
+		estacionamento.datahora_inclusao = new Date();
 
 		var connection = app.persistence.connectionFactory();
 		var estacionamentoDAO = new app.persistence.EstacionamentoDAO(connection);
@@ -92,9 +106,7 @@ module.exports = function(app) {
 				console.log('Erro ao criar estacionamento: ' + erro);
 				res.status(500).send(erro);
 			} else {
-				if (resultado.insertId) {
-					estacionamento.id = resultado.insertId;
-				}
+				estacionamento.id = resultado.insertId;
 
 				for (var x in estacionamentoValores) {
 					estacionamentoValores[x].id_estacionamento = estacionamento.id;
@@ -105,11 +117,60 @@ module.exports = function(app) {
 
 					estacionamentoDAO.salvaValores(estacionamentoValores[x], function(erroValores, resultadoValores){
 						if (erroValores) {
-							console.log('Erro ao criar Valores: ' + erroValores);
+							console.log('Erro ao salvar Valores: ' + erroValores);
 						}
 					});
 				}
-				res.status(201).json(resultado);
+				res.status(201).json(estacionamento);
+			}
+		});
+	});
+
+	app.put('/estacionamentos', function(req, res){
+		console.log('Processando uma atualização de estacionamento.');
+
+		req.assert("estacionamento.nome", "O nome do estacionamento é obrigatório.").notEmpty();
+		req.assert("estacionamento.id_usuario", "O usuário é obrigatório").notEmpty();
+
+		var erros = req.validationErrors();
+
+		if (erros) {
+			console.log('Erros de validação encontrados.');
+			res.status(400).send(erros);
+			return;
+		}
+
+		var estacionamento = req.body["estacionamento"];
+		
+		estacionamento.datahora_alteracao = new Date();
+		delete estacionamento.datahora_inclusao;
+
+		var connection = app.persistence.connectionFactory();
+		var estacionamentoDAO = new app.persistence.EstacionamentoDAO(connection);
+
+		var estacionamentoValores = estacionamento.estacionamentoValores;
+		delete estacionamento.estacionamentoValores;
+
+		estacionamentoDAO.atualiza(estacionamento, function(erro, resultado){
+			if (erro) {
+				console.log('Erro ao atualizar estacionamento: ' + erro);
+				res.status(500).send(erro);
+			} else {
+
+				for (var x in estacionamentoValores) {
+					estacionamentoValores[x].id_estacionamento = estacionamento.id;
+					delete estacionamentoValores[x].descricao;
+
+					var connection = app.persistence.connectionFactory();
+					var estacionamentoDAO = new app.persistence.EstacionamentoDAO(connection);
+
+					estacionamentoDAO.atualizaValores(estacionamentoValores[x], function(erroValores, resultadoValores){
+						if (erroValores) {
+							console.log('Erro ao atualizar Valores: ' + erroValores);
+						}
+					});
+				}
+				res.status(201).json(estacionamento);
 			}
 		});
 	});
--- persistence/EstacionamentoDAO.js
+++ persistence/EstacionamentoDAO.js
@@ -4,27 +4,25 @@ function EstacionamentoDAO(pool) {
 
 EstacionamentoDAO.prototype.salva = function(estacionamento, callback) {
 	this._pool.getConnection(function(err, connection){
-		if (estacionamento.id) {
-			connection.query('UPDATE estacionamentos SET ? WHERE id = ?', [estacionamento, estacionamento.id], callback);
-		} else {
-			connection.query('INSERT INTO estacionamentos SET ?', estacionamento, callback);
-		}
-	});
-}
-
-EstacionamentoDAO.prototype.salvaValores = function(estacionamentoValores, callback) {
-	this._pool.getConnection(function(err, connection){
-		if (estacionamentoValores.id) {
-			connection.query('UPDATE estacionamentos_valores SET ? WHERE id = ?', [estacionamentoValores, estacionamentoValores.id], callback);
-		} else {
-			connection.query('INSERT INTO estacionamentos_valores SET ?', estacionamentoValores, callback);
-		}
+		connection.query('INSERT INTO estacionamentos SET ?', estacionamento, callback);
 	});
 }
 
 EstacionamentoDAO.prototype.atualiza = function(estacionamento, callback) {
 	this._pool.getConnection(function(err, connection){
-		connection.query('UPDATE estacionamentos SET status = ? WHERE id = ?', [estacionamento.status, estacionamento.id], callback);
+		connection.query('UPDATE estacionamentos SET ? WHERE id = ?', [estacionamento, estacionamento.id], callback);
+	});
+}
+
+EstacionamentoDAO.prototype.salvaValores = function(estacionamentoValores, callback) {
+	this._pool.getConnection(function(err, connection){
+		connection.query('INSERT INTO estacionamentos_valores SET ?', estacionamentoValores, callback);
+	});
+}
+
+EstacionamentoDAO.prototype.atualizaValores = function(estacionamentoValores, callback) {
+	this._pool.getConnection(function(err, connection){
+		connection.query('UPDATE estacionamentos_valores SET ? WHERE id = ?', [estacionamentoValores, estacionamentoValores.id], callback);
 	});
 }
 
@@ -42,7 +40,7 @@ EstacionamentoDAO.prototype.buscaPorId = function(id, callback) {
 
 EstacionamentoDAO.prototype.listaPorIdUsuario = function(id_usuario, callback) {
 	this._pool.getConnection(function(err, connection){
-		connection.query('SELECT distinct * FROM estacionamentos WHERE id_usuario = ?', id_usuario, callback);
+		connection.query('SELECT * FROM estacionamentos WHERE id_usuario = ?', id_usuario, callback);
 	});
 }
 
