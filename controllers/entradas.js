15bf3dd4029073b6ecbeefeea34de5b37dbbe5a9
ricardocrispim <ripadua7@gmail.com>
2017-06-13 23:50:57 -0300

    Correção de parse das datas

--- controllers/entradas.js
+++ controllers/entradas.js
@@ -20,7 +20,11 @@ module.exports = function(app) {
 		}
 
 		var entrada = req.body["entrada"];
-		entrada.datahora_entrada = new Date(entrada.datahora_entrada);
+		var partes = entrada.datahora_entrada.split(" ");
+		var data = partes[0].split("/");
+		var hora = partes[1].split(":");
+		entrada.datahora_entrada = new Date(data[2], data[1]-1, data[0], hora[0], hora[1], hora[2], 0);
+		
 		console.log(entrada);
 
 		var connection = app.persistence.connectionFactory();
@@ -60,8 +64,17 @@ module.exports = function(app) {
 		}
 
 		var entrada = req.body["entrada"];
-		entrada.datahora_entrada = new Date(entrada.datahora_entrada);
-		entrada.datahora_saida = new Date(entrada.datahora_saida);
+		var partes = entrada.datahora_entrada.split(" ");
+		var data = partes[0].split("/");
+		var hora = partes[1].split(":");
+		entrada.datahora_entrada = new Date(data[2], data[1]-1, data[0], hora[0], hora[1], hora[2], 0);
+		
+		partes = entrada.datahora_saida.split(" ");
+		data = partes[0].split("/");
+		hora = partes[1].split(":");
+		entrada.datahora_saida = new Date(data[2], data[1]-1, data[0], hora[0], hora[1], hora[2], 0);
+		
+		console.log(entrada);
 
 		var connection = app.persistence.connectionFactory();
 		var entradaDAO = new app.persistence.EntradaDAO(connection);
