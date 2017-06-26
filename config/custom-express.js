5320e8ce66b531294683f5c531ff89c6cbea8e5e
ripadua <ripadua7@gmail.com>
2017-06-26 14:09:59 -0300

    Incluindo template AdminLTE

--- config/custom-express.js
+++ config/custom-express.js
@@ -20,6 +20,8 @@ module.exports = function() {
 
 	app.use(expressValidator());
 
+	app.use(express.static('public'));
+
 	app.use(function(req, res, next) {
 	  res.header("Access-Control-Allow-Origin", "*");
 	  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
